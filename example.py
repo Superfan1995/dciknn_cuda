2f674becb4deb4b8fa1f7162170053a33bf94fe1
Superfan1995 <xfa24@sfu.ca>
2023-05-01 22:07:58 -0700

    Update example.py

--- example.py
+++ example.py
@@ -105,7 +105,7 @@ def main():
         #print("Indices Distances:", dists)
         dci_db.clear()
         #b = datetime.datetime.now()
-        #print(b-a)    
+        #print(b-a) 
 
 if __name__ == '__main__':
     main()
