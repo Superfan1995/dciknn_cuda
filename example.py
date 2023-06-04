a5ddec4b33215f0ac28882f20ee46d42356200d9
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-06-04 16:39:19 -0700

    update setup file for adding g and G flag

--- example.py
+++ example.py
@@ -93,11 +93,11 @@ def main():
         a = datetime.datetime.now()
         dci_db = DCI(num_heads, dim, num_comp_indices, num_simp_indices, block_size, thread_size, device=0)
 
-        #dci_db.add(data)
+        dci_db.add(data)
         #indices, dists = dci_db.query(query, num_neighbours, num_outer_iterations)
         #print("Nearest Indices:", indices)
         #print("Indices Distances:", dists)
-        #dci_db.clear()
+        dci_db.clear()
         b = datetime.datetime.now()
         print(b-a) 
 
