146916a12477980b019226ec8fb41ed1dccfaf92
Superfan1995 <35352971+Superfan1995@users.noreply.github.com>
2023-06-18 20:58:28 -0700

    Update dci_cuda_kernel.cu

--- src/dci_cuda_kernel.cu
+++ src/dci_cuda_kernel.cu
@@ -109,7 +109,7 @@ void dci_init(dci* const dci_inst, const int num_heads, const int dim, const int
 		const int num_simp_indices, const int devId) {
 
 	printf("dci_init success inside\n");
-	//printf("Hello world!\n");
+	cudaDeviceSynchronize();
 
 	int num_indices = num_comp_indices * num_simp_indices;
 
@@ -190,7 +190,10 @@ __global__ void copy_to_indices(dci* const dci_inst, float* const data_proj,
 void dci_add(dci* const dci_inst, const int num_heads, const int dim, const int num_points,
 		float* const data, const int block_size, const int thread_size) {
 
+	//cudaDeviceSynchronize();
 	printf("dci_add inside\n");
+	//printf("a=10, b=2\n");
+	cudaDeviceSynchronize();
 
 	int num_indices = dci_inst->num_comp_indices * dci_inst->num_simp_indices;
 	float *data_proj;
